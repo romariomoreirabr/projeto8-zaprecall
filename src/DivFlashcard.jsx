f4021c43e10264b84dc51787603f7e45a14b2121
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-03-24 09:18:32 -0300

    Preparando revisão de código

--- src/DivFlashcard.jsx
+++
@@ -1,8 +0,0 @@
-export default function DivFlashcard(props) {
-    return (
-        <div className="flashcard">
-            <span>Pergunta {props.contador}</span>
-            <img src="./img/play.svg" alt="play" />
-        </div>
-    )
-}
--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header>
-            <img src="./img/logo.svg" alt="logo" />
-            <span>ZapRecall</span>
-        </header>
-    )
-}
